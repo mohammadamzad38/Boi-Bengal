55de2d75d6737c79897dc8a5067e587da3361734
mohammadamzad38 <hossainamzad38@gmail.com>
2024-12-11 16:31:28 +0600

    finish

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Book = ({ book }) => {
-  const {bookId, bookName, image, author, tags, category } = book;
+  const {bookId, bookName, image, author, tags, category, rating, totalPages } = book;
   return (
     <Link to={`/books/${bookId}`}>
       <div className="card bg-base-100 w-96 shadow-xl p-6 ">
@@ -25,6 +25,8 @@ const Book = ({ book }) => {
           <div className="border border-dashed"></div>
           <div className="card-actions justify-between">
             <div className="badge badge-outline">{category}</div>
+            <div className="font-bold text-xs">{rating} <br /> <span>Page: {totalPages}</span></div>
+            
             <div className="rating space-x-3">
               <input
                 type="radio"
--- src/Components/Pages/ListedBooks.jsx
+++ src/Components/Pages/ListedBooks.jsx
@@ -6,13 +6,14 @@ import { getStoredReadList } from "./AddToDBList";
 import Book from "../Book";
 
 const ListedBooks = () => {
+  const [sort, setSort] = useState("");
   const [readlist, setReadList] = useState([]);
   const allBooks = useLoaderData();
   useEffect(() => {
     const storedList = getStoredReadList();
     console.log("store list return", storedList);
     const storedListInt = storedList.map((id) => parseInt(id));
-    console.log('booksssalllllll',allBooks);
+    console.log("booksssalllllll", allBooks);
 
     const readBookList = allBooks.filter((book) =>
       storedListInt.includes(book.bookId)
@@ -20,9 +21,37 @@ const ListedBooks = () => {
     setReadList(readBookList);
   }, []);
 
+  const handleSort = (sortType) => {
+    setSort(sortType);
+
+    if( sortType === 'No of Pages'){
+      const sortedReadList = [...readlist].sort((a, b) => b.totalPages - a.totalPages);
+      setReadList(sortedReadList)
+    }
+
+    if(sortType === 'Ratings'){
+      const storedratingsList = [...readlist].sort((a, b) => a.totalPages - b.totalPages);
+      setReadList(storedratingsList)
+    }
+  };
+
   return (
     <div>
       <h3 className="text-3xl my-8">Listed Books</h3>
+
+      <details className="dropdown mb-16">
+        <summary className="btn m-1">
+          {sort ? `sort By: ${sort}` : "Sort By"}
+        </summary>
+        <ul className="menu dropdown-content bg-base-100 rounded-box z-[1] w-52 p-2 shadow">
+          <li onClick={() => handleSort('Ratings')}>
+            <a>Ratings</a>
+          </li>
+          <li onClick={() => handleSort('No of Pages')}>
+            <a>No of Pages</a>
+          </li>
+        </ul>
+      </details>
 
       <Tabs>
         <TabList>
